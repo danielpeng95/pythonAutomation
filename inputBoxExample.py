676ca39391ac09cffc0b9b9d52091db50b0c482f
danielpeng95 <danielpeng95@yahoo.com>
2019-11-23 10:59:26 -0500

    add and delete

--- inputBoxExample.py
+++ inputBoxExample.py
@@ -1,13 +1,10 @@
 from selenium import webdriver
 from selenium.webdriver.common.by import By
-from selenium.webdriver.common.keys import Keys
 import unittest
 import time
-from selenium.webdriver.support import expected_conditions as EC
-from selenium.webdriver.support.ui import WebDriverWait
 
 
-class ExampleInputBox(unittest.TestCase):
+class Example(unittest.TestCase):
 
     def setUp(self):
         self.driver = webdriver.Chrome("C:\\Users\\benpe\\DevMountain\\testing-resources\\chromedriver.exe")
@@ -18,19 +15,22 @@ class ExampleInputBox(unittest.TestCase):
     def tearDown(self):
         self.driver.close()
 
-    def Example(self):
+    def test_challenge(self):
         self.driver.get("https://fs2.formsite.com/meherpavan/form2/index.html?1537702596407")
         self.driver.implicitly_wait(10)
 
         inputboxes = self.driver.find_elements(By.CLASS_NAME, 'text_field')
         print(len(inputboxes))
 
+        status = self.driver.find_element(By.ID, 'RESULT_TextField-1').is_displayed()
+        print(status)
+
+        status = self.driver.find_element(By.ID, 'RESULT_TextField-1').is_enabled()
+        print(status)
+
         self.driver.find_element(By.ID, 'RESULT_TextField-1').send_keys("Daniel")
         self.driver.find_element(By.ID, 'RESULT_TextField-2').send_keys("Peng")
         self.driver.find_element_by_id("RESULT_TextField-3").send_keys("1234567890")
-
-
-
         time.sleep(5)
 
 
