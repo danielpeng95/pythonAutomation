b35857aa91242de4c6b00f5163490207efb07c53
danielpeng95 <danielpeng95@yahoo.com>
2019-12-01 17:01:54 -0500

    add mouse hover

--- Example9MouseActions.py
+++ Example9MouseActions.py
@@ -19,9 +19,21 @@ class Example(unittest.TestCase):
         self.driver.get("https://opensource-demo.orangehrmlive.com")
         self.driver.implicitly_wait(10)
 
-        self.driver.find_element_by_xpath('//*[@id="divUsername"]/span').send_keys("Admin")
+        #sign into the website
+        self.driver.find_element_by_xpath('//input[@name="txtUsername"]').send_keys("Admin")
         self.driver.find_element_by_xpath('//*[@id="txtPassword"]').send_keys("admin123")
         self.driver.find_element_by_xpath('//*[@id="btnLogin"]').click()
+
+        admin = self.driver.find_element_by_xpath('//*[@id="menu_admin_viewAdminModule"]/b')
+        usermgnt = self.driver.find_element_by_xpath('//*[@id="menu_admin_UserManagement"]')
+        users = self.driver.find_element_by_xpath('//*[@id="menu_admin_viewSystemUsers"]')
+
+        #mouseover/mousehover action
+        actions = ActionChains(self.driver)
+        actions.move_to_element(admin).move_to_element(usermgnt).move_to_element(users).click().perform()
+
+
+
 
 
         time.sleep(3)
