9eeb4cba5570d0d82d03fb4bbc9f3d6423c8bf8f
danielpeng95 <danielpeng95@yahoo.com>
2019-12-01 16:49:40 -0500

    sign into the website

---
+++ Example9MouseActions.py
@@ -0,0 +1,29 @@
+from selenium import webdriver
+import unittest
+import time
+from selenium.webdriver import ActionChains
+
+
+class Example(unittest.TestCase):
+
+    def setUp(self):
+        self.driver = webdriver.Chrome("C:\\Users\\benpe\\DevMountain\\testing-resources\\chromedriver.exe")
+        self.driver.implicitly_wait(20)
+        self.driver.set_page_load_timeout(20)
+        self.driver.maximize_window()
+
+    def tearDown(self):
+        self.driver.close()
+
+    def test_challenge(self):
+        self.driver.get("https://opensource-demo.orangehrmlive.com")
+        self.driver.implicitly_wait(10)
+
+        self.driver.find_element_by_xpath('//*[@id="divUsername"]/span').send_keys("Admin")
+        self.driver.find_element_by_xpath('//*[@id="txtPassword"]').send_keys("admin123")
+
+
+        time.sleep(3)
+
+if __name__ == '__main__':
+    unittest.main()
