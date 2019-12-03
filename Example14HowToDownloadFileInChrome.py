6731ae2f665daa23d3df3c97af248f152aa37c40
danielpeng95 <danielpeng95@yahoo.com>
2019-12-03 17:24:47 -0500

    add the code for How to download file in chrome browser

---
+++ Example14HowToDownloadFileInChrome.py
@@ -0,0 +1,30 @@
+from selenium import webdriver
+import unittest
+import time
+from selenium.webdriver import ActionChains
+
+
+class Example(unittest.TestCase):
+
+    def setUp(self):
+        self.driver = webdriver.Chrome("C:\\Users\\benpe\\DevMountain\\testing-resources\\chromedriver.exe")
+        self.driver.implicitly_wait(20)
+        self.driver.set_page_load_timeout(20)
+        self.driver.maximize_window()
+
+    def tearDown(self):
+        self.driver.close()
+
+    def test_challenge(self):
+        self.driver.get("http://demo.automationtesting.in/FileDownload.html")
+        self.driver.implicitly_wait(10)
+
+        #download text file
+        self.driver.find_element_by_xpath('//textarea[@id="textbox"]').send_keys("Testing")
+        self.driver.find_element_by_xpath('//button[@id="createTxt"]').click() #generate file button
+        self.driver.find_element_by_xpath('//a[@download="info.txt"]').click() #download link
+
+        time.sleep(3)
+
+if __name__ == '__main__':
+    unittest.main()
