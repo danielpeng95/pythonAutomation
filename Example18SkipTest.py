453bca2bf397465af9bb476379c4802345eb6eae
danielpeng95 <danielpeng95@yahoo.com>
2019-12-05 16:14:56 -0500

    how to skip test in unittest framework

--- Example18SkipTest.py
+++ Example18SkipTest.py
@@ -7,9 +7,13 @@ class AppTesting(unittest.TestCase):
     def test_search(self):
         print("This is search test")
 
+    #this will skip test, but print a message
+    @unittest.skip("I am skipping this test method because it is not yet ready")
     def test_advancedSearch(self):
         print("this is advanced search method")
 
+    #will skip on "if" condition
+    @unittest.skipIf(1==1, "skip test if one is equal to one")
     def test_prepaidRecharge(self):
         print("this is prepaid recharge")
 
